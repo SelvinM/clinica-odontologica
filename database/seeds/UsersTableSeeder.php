7892e58dad9cd18309f4cbc7579c6e1eb5458182
SelvinM <sel2230@gmail.com>
2019-02-05 22:05:32 -0600

    bootstrap + seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'role_id' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@correo.com',
+            'password' => bcrypt('secret'), // secret
+            'remember_token' => str_random(10),
+        ]);
+    }
+}
