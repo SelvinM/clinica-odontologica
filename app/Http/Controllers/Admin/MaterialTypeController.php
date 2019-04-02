27ef637a8bb0ed9843d88e400c0549e5eecbd73b
FROST <denisadonis06@gmail.com>
2019-04-02 10:59:23 -0600

    modificacion de los formularios de tipos de marteriales y el Controller

--- app/Http/Controllers/Admin/MaterialTypeController.php
+++ app/Http/Controllers/Admin/MaterialTypeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class MaterialController extends Controller
+class MaterialTypeController extends Controller
 {
     /**
      * Display a listing of the resource.
