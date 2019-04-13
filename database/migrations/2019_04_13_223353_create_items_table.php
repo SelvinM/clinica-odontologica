841e0d3dd111e2abee28ed34650c3c3d9338408c
SelvinM <sel2230@gmail.com>
2019-04-13 16:34:22 -0600

    migraciones nuevas

--- database/migrations/2019_04_13_223353_create_items_table.php
+++ database/migrations/2019_04_13_223353_create_items_table.php
@@ -21,7 +21,10 @@ class CreateItemsTable extends Migration {
 			$table->decimal('price', 10);
 			$table->decimal('cost', 10);
 			$table->integer('quantity');
+			$table->integer('batch');
+			$table->dateTime('purchase_date')->nullable();
 			$table->dateTime('expiration_date')->nullable();
+			$table->string('description', 500)->nullable();
 			$table->timestamps();
 			$table->softDeletes();
 		});
