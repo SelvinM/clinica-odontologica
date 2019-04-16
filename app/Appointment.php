46877b7db9112ce3f8b930b0ba2bcb7e8d89652e
JuanSoler <juansoler631@gmail.com>
2019-04-15 21:13:01 -0600

    numeral agregado

--- app/Appointment.php
+++ app/Appointment.php
@@ -80,7 +80,7 @@ class Appointment extends Eloquent
             ->get();
 		}
             
-            $json_arr=array($appointments);
+            #$json_arr=array($appointments);
             foreach ($appointments as $appointment) {
                 $json_arr[]=array("id"=>$appointment->id,"title"=>$appointment->namepatient,"description"=>$appointment->description,"start"=>"".$appointment->date."","editable"=>false);
             } 
