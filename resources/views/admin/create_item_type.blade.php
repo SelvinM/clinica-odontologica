1184576a94c360f5fd84a523aa92317b06c38ddd
leyquel <leyquel@gmail.com>
2019-04-17 14:53:32 -0600

    Cambio de label inventario por producto (vista crear tipo de producto)

--- resources/views/admin/create_item_type.blade.php
+++ resources/views/admin/create_item_type.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app1')
 @section('toggle')
-<a href="{{ route('admin item types') }}" class="btn btn-secondary">← Tipos de inventario</a>
+<a href="{{ route('admin item types') }}" class="btn btn-secondary">← Tipos de producto</a>
 @endsection
 @section('content')
 <div class="container form-sm">
@@ -10,7 +10,7 @@
 				<td>
 					<form class="well form-horizontal" method="post" action="{{route('admin store item type')}}">
 						@csrf
-						<legend>Nuevo tipo de inventario</legend>
+						<legend>Nuevo tipo de producto</legend>
 						<div class="form-group input-group">
 							<div class="input-group-prepend">
 								<span class="input-group-text"> <i class="fa  fa-flask"></i> </span>
