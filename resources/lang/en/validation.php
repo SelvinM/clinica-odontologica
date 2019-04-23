2b737df0d9dbf18dddbfecbb8ce6d12432513b84
SelvinM <sel2230@gmail.com>
2019-04-22 22:03:26 -0600

    Update validation.php

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -95,7 +95,7 @@ return [
     'present' => 'The :attribute field must be present.',
     'regex' => 'The :attribute format is invalid.',
     'required' => 'Este campo es requerido.',
-    'required_if' => 'The :attribute field is required when :other is :value.',
+    'required_if' => 'ee :attribute field is required when :other is :value.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
     'required_with' => 'The :attribute field is required when :values is present.',
     'required_with_all' => 'The :attribute field is required when :values are present.',
