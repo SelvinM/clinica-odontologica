2dc0a5e02388505ddea0d0cb59d7b8620734b8fd
SelvinM <sel2230@gmail.com>
2019-04-22 22:01:47 -0600

    cambio a espaniol... validacion en login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Las credenciales no concuerdan.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor intentar mas tarde.',
 
 ];
